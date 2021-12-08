8ef09a8bdbbd9cb91080696108ed9627abdd05b7
Nikolay Zobnev <info@kolyablack.ru>
2021-12-08 12:59:00 +0300

    fixes

--- components/KeyClients.vue
+++ components/KeyClients.vue
@@ -2,8 +2,8 @@
   <div v-intersect="{ in: ['animate-line'] }" class="key-clients top-border">
     <div v-intersect="{ in: ['fade-in'] }" class="block-title opacity-0">Ключевые клиенты</div>
 
-    <div class="client-list">
-      <div v-for="client in clients" :key="client.id" v-intersect="{ in: ['fade-in'] }" class="client-item opacity-0">
+    <div v-intersect="{ in: ['fade-in'] }" class="client-list opacity-0">
+      <div v-for="client in clients" :key="client.id" class="client-item">
         <div class="client-name">
           {{ client.Name }}
         </div>
@@ -11,7 +11,8 @@
           {{ client.Description }}
         </div>
         <div v-if="client.Image" class="client-img">
-          <img :src="$strapi.options.url + client.Image.url" alt="">
+          <!-- <img :src="$strapi.options.url + client.Image.url" alt=""> -->
+          <img src='~/assets/images/client-img.jpg' alt="">
         </div>
       </div>
     </div>
@@ -26,9 +27,10 @@ export default {
       error: null
     }
   },
-  async mounted() {
+  mounted() {
     try {
-      this.clients = await this.$strapi.$clients.find()
+      // this.clients = await this.$strapi.$clients.find()
+      this.clients = [{"id":1,"Name":"Кубань вино","Description":"description","published_at":"2021-11-17T09:25:28.078Z","created_at":"2021-11-17T09:25:09.547Z","updated_at":"2021-11-17T09:25:28.092Z","Image":{"id":1,"name":"Rectangle 737.jpg","alternativeText":"","caption":"","width":477,"height":302,"formats":{"thumbnail":{"name":"thumbnail_Rectangle 737.jpg","hash":"thumbnail_Rectangle_737_e78b9499b6","ext":".jpg","mime":"image/jpeg","width":245,"height":155,"size":6.04,"path":null,"url":"/uploads/thumbnail_Rectangle_737_e78b9499b6.jpg"}},"hash":"Rectangle_737_e78b9499b6","ext":".jpg","mime":"image/jpeg","size":14.11,"url":"/uploads/Rectangle_737_e78b9499b6.jpg","previewUrl":null,"provider":"local","provider_metadata":null,"created_at":"2021-11-17T09:24:48.798Z","updated_at":"2021-11-17T09:24:48.807Z"}},{"id":2,"Name":"Кубань вино","Description":"description","published_at":"2021-11-17T09:25:42.277Z","created_at":"2021-11-17T09:25:40.564Z","updated_at":"2021-11-17T09:25:42.298Z","Image":{"id":1,"name":"Rectangle 737.jpg","alternativeText":"","caption":"","width":477,"height":302,"formats":{"thumbnail":{"name":"thumbnail_Rectangle 737.jpg","hash":"thumbnail_Rectangle_737_e78b9499b6","ext":".jpg","mime":"image/jpeg","width":245,"height":155,"size":6.04,"path":null,"url":"/uploads/thumbnail_Rectangle_737_e78b9499b6.jpg"}},"hash":"Rectangle_737_e78b9499b6","ext":".jpg","mime":"image/jpeg","size":14.11,"url":"/uploads/Rectangle_737_e78b9499b6.jpg","previewUrl":null,"provider":"local","provider_metadata":null,"created_at":"2021-11-17T09:24:48.798Z","updated_at":"2021-11-17T09:24:48.807Z"}},{"id":3,"Name":"Кубань вино","Description":"description","published_at":"2021-11-17T09:25:55.699Z","created_at":"2021-11-17T09:25:54.219Z","updated_at":"2021-11-17T09:25:55.711Z","Image":{"id":1,"name":"Rectangle 737.jpg","alternativeText":"","caption":"","width":477,"height":302,"formats":{"thumbnail":{"name":"thumbnail_Rectangle 737.jpg","hash":"thumbnail_Rectangle_737_e78b9499b6","ext":".jpg","mime":"image/jpeg","width":245,"height":155,"size":6.04,"path":null,"url":"/uploads/thumbnail_Rectangle_737_e78b9499b6.jpg"}},"hash":"Rectangle_737_e78b9499b6","ext":".jpg","mime":"image/jpeg","size":14.11,"url":"/uploads/Rectangle_737_e78b9499b6.jpg","previewUrl":null,"provider":"local","provider_metadata":null,"created_at":"2021-11-17T09:24:48.798Z","updated_at":"2021-11-17T09:24:48.807Z"}},{"id":4,"Name":"Кубань вино","Description":"description","published_at":"2021-11-17T09:26:01.693Z","created_at":"2021-11-17T09:25:59.997Z","updated_at":"2021-11-17T09:26:01.704Z","Image":{"id":1,"name":"Rectangle 737.jpg","alternativeText":"","caption":"","width":477,"height":302,"formats":{"thumbnail":{"name":"thumbnail_Rectangle 737.jpg","hash":"thumbnail_Rectangle_737_e78b9499b6","ext":".jpg","mime":"image/jpeg","width":245,"height":155,"size":6.04,"path":null,"url":"/uploads/thumbnail_Rectangle_737_e78b9499b6.jpg"}},"hash":"Rectangle_737_e78b9499b6","ext":".jpg","mime":"image/jpeg","size":14.11,"url":"/uploads/Rectangle_737_e78b9499b6.jpg","previewUrl":null,"provider":"local","provider_metadata":null,"created_at":"2021-11-17T09:24:48.798Z","updated_at":"2021-11-17T09:24:48.807Z"}}]
     } catch (error) {
       this.error = error
     }
@@ -48,7 +50,16 @@ export default {
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  padding: 20px 0;
+  padding: 1.3vw 0;
+  @media #{$media-lg} {
+    padding: 1.5vw 0;
+  }
+  @media #{$media-md} {
+    padding: 1.8vw 0;
+  }
+  @media #{$media-xs} {
+    padding: 20px 0;
+  }
 
 
   @media #{$media-sm-up} {
@@ -70,22 +81,36 @@ export default {
 
   .client-name {
     cursor: pointer;
-    font-size: $font-20;
+    font-size: 2vw;
     color: $white;
 
     @media #{$media-sm-up} {
       color: $blackGray;
-      font-size: $font-32;
       width: 816px;
     }
+    @media #{$media-lg} {
+      font-size: 2.2vw;
+    }
+    @media #{$media-md} {
+      font-size: 2.4vw;
+    }
+    @media #{$media-xs} {
+      font-size: 20px;
+    }
+
   }
 
   .client-desc {
     color: $blackGray;
-    font-size: $font-12;
+    font-size: 1.4vw;
     @media #{$media-sm-up} {
-      font-size: $font-18;
       width: 480px;
+    }
+    @media #{$media-md} {
+      font-size: 1.8vw;
+    }
+    @media #{$media-xs} {
+      font-size: $font-12;
     }
   }
 
